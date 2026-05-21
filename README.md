7b737f73c32031b1aca03fe9425a770fc5f8829e
Mahesh-0046 <krishnamahesh0046@gmail.com>
2026-05-21 17:51:40 +0530

    Day 1: Python basics

---
+++ README.md
@@ -0,0 +1,17 @@
+# Day 1 – Python Basics
+
+## Topics Learned
+- print()
+- Variables
+- if-else
+- Indentation
+
+## Programs Practiced
+1. Print statements
+2. Conditional statements
+3. Variables
+
+## Key Learnings
+- Python uses indentation
+- Variables store data
+- if-else controls logic
---
+++ main.py
@@ -0,0 +1,14 @@
+# Start with a basic print function
+print("First python function")
+
+# Indentation
+if 5 > 2:
+    print("Five is greater than two")
+else:
+    print("Invalid entry")
+
+# Variables
+name = "Mahesh"
+date = "12-6-2026"
+
+print("My name is:", name, "and today's date is:", date)
