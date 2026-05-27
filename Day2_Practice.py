9e972948281e85d9626c31e0a70746478f135a7f
Krishna Mahesh <krishnamahesh0046@gmail.com>
2026-05-27 17:17:07 +0530

    Add files via upload

---
+++ Day2_Practice.py
@@ -0,0 +1,24 @@
+#1. for_loop.py
+for i in range(5):
+    print(i)
+
+for i in range(1, 10, 2):
+    print(i)
+
+#2. while_loop.py
+count = 1
+
+while count <= 5:
+    print(count)
+    count += 1
+
+for number in range(1, 11):
+    print(number)
+
+#3. range_function.py
+for number in range(1, 11):
+    print(number)
+
+#4. patterns.py
+for i in range(5):
+    print("*" * (i + 1))
---
+++ Day2_README.md
@@ -0,0 +1,8 @@
+PYTHON-Learning/
+│
+└── Day2/
+    ├── for_loop.py
+    ├── while_loop.py
+    ├── range_function.py
+    ├── patterns.py
+    └── README.md
